82d53256d26f50235ea35fb4a76975326716cc83
Fikrii2806 <115148345+Fikrii2806@users.noreply.github.com>
2022-11-03 18:03:28 +0700

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,8 @@
+const loginButton = document.getElementById("login-button");
+loginButton.addEventListener("click", function () {
+  const emailField = document.getElementById("email");
+  const passwordField = document.getElementById("password");
+  const emailText = emailField.value;
+  const passwordText = passwordField.value;
+  }
+);
